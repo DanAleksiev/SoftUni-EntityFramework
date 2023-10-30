7a4df3656f25ecb3ce95e52ba835040b8193a0db
DanAleksiev <toksikar46@gmail.com>
2023-10-30 08:35:40 +0000

    ..

---
+++ ExercisesIntroductionToEntityFramework/ExercisesIntroductionToEntityFramework/Program.cs
@@ -0,0 +1,14 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine(context.Employees.FirstOrDefault().FirstName.ToString());
+            // Scaffold-DbContext "Server=MSI\SQLEXPRESS;Database=SoftUni;Integrated Security=True;TrustServerCertificate=True" Microsoft.EntityFrameworkCore.SqlServer -o Models -d
+            }
+        }
+    }
---
+++ SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+
+namespace SoftUni
+    {
+    public class StartUp
+        {
+        static void Main(string[] args)
+            {
+
+            // Scaffold - DbContext "Server=MSI\SQLEXPRESS;Database=SoftUni;Integrated Security=True;TrustServerCertificate=True" Microsoft.EntityFrameworkCore.SqlServer - o Models - d
+            // cls to lean the manager
+
+            //Scaffold-DbContext -Connection "Server=MSI\SQLEXPRESS;Database=SoftUni;Integrated Security=True;TrustServerCertificate=True" -Provider Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data/Models
+
+
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(context));
+            }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+            {
+            var employees = context.Employees
+                .Select(e => new
+                    {
+                        e.FirstName,
+                        e.LastName,
+                        e.MiddleName,
+                        e.JobTitle,
+                        e.Salary
+                    }).ToList();
+
+
+            string result = string.Join(Environment.NewLine, employees.Select( e=> $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}"));
+            return result;
+            }
+        }
+    }
