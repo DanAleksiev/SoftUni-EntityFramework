444ac2e624dc5edd85045c114e9823012ee595b4
DanAleksiev <toksikar46@gmail.com>
2023-11-01 10:14:00 +0000

    ...

---
+++ EntityRealations/P01_StudentSystem.Data.Models/Students.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_StudentSystem.Data.Models
+    {
+    public class Students
+        {
+        public int StudentId { get; set; }
+        [MaxLength(80)]
+        [Unicode]
+        [Required]
+        public string Name { get; set;}
+        [MaxLength(10)]
+        public string Phonenumber { get; set;}
+        [Required]
+        public DateTime RegisteredOn { get; set; }
+    }
+    }
