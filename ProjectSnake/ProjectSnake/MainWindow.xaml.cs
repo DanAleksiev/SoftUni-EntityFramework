191366ae5f5c1436de955ca7260007a3e2a5de8b
DanAleksiev <toksikar46@gmail.com>
2023-11-11 18:53:09 +0000

    implamentMovement

--- ProjectSnake/ProjectSnake/MainWindow.xaml.cs
+++ ProjectSnake/ProjectSnake/MainWindow.xaml.cs
@@ -20,9 +20,97 @@ namespace ProjectSnake
     /// </summary>
     public partial class MainWindow : Window
         {
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+            {
+                {GridValue.Empty, Images.Empty },
+                {GridValue.Snake, Images.Body },
+                {GridValue.Food, Images.Food }
+
+            };
+
+
+        private readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImage;
+        private State gameState;
+
         public MainWindow()
             {
             InitializeComponent();
+            gridImage = SetupGrid();
+            gameState = new State(rows,cols);
+            }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+            {
+            Draw();
+            }
+        
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+            {
+            if(gameState.GameOver)
+                {
+                return;
+                }
+            switch(e.Key)
+                {
+                case Key.Left:
+                    gameState.ChangeDirection(Direction.Left); break;
+                case Key.Right:
+                    gameState.ChangeDirection(Direction.Right); break;
+                case Key.Up:
+                    gameState.ChangeDirection(Direction.Up); break;
+                case Key.Down:
+                    gameState.ChangeDirection(Direction.Down); break;
+                }
+            }
+
+        private async Task GameLoop()
+            {
+            while(!gameState.GameOver)
+                {
+                await Task.Delay(100);
+                }
+            }
+
+        private Image[,] SetupGrid()
+            {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            for (int r = 0; r < rows; r++)
+                {
+                for (int c = 0; c < cols; c++)
+                    {
+                    Image image = new Image
+                        {
+                        Source = Images.Empty
+                        };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                    }
+
+                }
+
+            return images;
+            }
+
+        private void Draw()
+            {
+            DrawGrid();
+            }
+
+        private void DrawGrid()
+            {
+            for(int r = 0;r < rows;r++)
+                {
+                for (int c = 0; c < cols; c++)
+                    {
+                    GridValue gridVal = gameState.Grid[r,c];
+                    gridImage[r, c].Source = gridValToImage[gridVal];
+                    }
+                }
             }
         }
     }
--- ProjectSnake/ProjectSnake/State.cs
+++ ProjectSnake/ProjectSnake/State.cs
@@ -31,7 +31,7 @@ namespace ProjectSnake
             {
             int r = Rows / 2;
 
-            for (int c = 0; c < 3; c++)
+            for (int c = 1; c <= 3; c++)
                 {
 
                 Grid[r, c] = GridValue.Snake;
