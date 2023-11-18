1997b47903c25633567d0b375a515461657c2dbd
DanAleksiev <toksikar46@gmail.com>
2023-11-18 22:14:09 +0000

    ...

---
+++ 09.XML/09.XML-Processing-Exercises-CarDealer-6.0/CarDealer/DTOs/Import/ImportCarsDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+    {
+    [XmlType("Car")]
+    public class ImportCarsDTO
+        {
+        [XmlElement("make")]
+        public string? Make { get; set; }
+
+        [XmlElement("model")]
+        public string? Model { get; set; }
+
+        [XmlElement("traveledDistance")]
+        public int traveledDistance { get; set; }
+
+        //[XmlArray("parts")]
+        public PartsCollection[] Parts { get; set; }
+        }
+
+    [XmlType("parts")]
+    public class PartsCollection
+        {
+        [XmlElement("partId")]
+        public int Id { get; set; }
+        }
+    }
---
+++ 09.XML/09.XML-Processing-Exercises-CarDealer-6.0/CarDealer/DTOs/Import/ImportSuppliersDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+    {
+    [XmlType("Supplier")]
+    public class ImportSuppliersDTO
+        {
+        [XmlElement("name")]
+        public string? Name { get; set; }
+
+        [XmlElement("isImported")]
+        public bool IsImported { get; set;}
+
+        }
+    }
