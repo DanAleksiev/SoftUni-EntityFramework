c5611170c6314fc8fcc0e33a032b7bdce8ad9155
DanAleksiev <toksikar46@gmail.com>
2023-11-24 08:48:02 +0000

    ...

--- examPrep/1/Invoices/Data/Configuration.cs
+++ examPrep/1/Invoices/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString = @"Server=.;Database=Invoices;Integrated Security=True;Encrypt=False";
+        public static string ConnectionString = "Server=MSI\\SQLEXPRESS;Database=CarDealerXlm;Integrated Security=True;MultipleActiveResultSets=true;Encrypt=False";
     }
 }
