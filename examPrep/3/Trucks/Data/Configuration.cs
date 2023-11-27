ca0ea1c93c732dffb0cc14b0156a69dceedb4638
DanAleksiev <toksikar46@gmail.com>
2023-11-27 23:51:49 +0000

    ...

---
+++ examPrep/3/Trucks/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Trucks.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=MSI\SQLEXPRESS;Database=CarDealerXlm;Integrated Security=True;MultipleActiveResultSets=true;Encrypt=False";
+        }
+}
