fc0268c4139c7b84e7b683ade4d603d6b71a2c34
DanAleksiev <toksikar46@gmail.com>
2023-11-27 14:49:22 +0000

    ...

---
+++ examPrep/2/Boardgames/DataProcessor/ExportDto/ExportSellersDTO.cs
@@ -0,0 +1,24 @@
+﻿using Boardgames.Data.Models;
+using Boardgames.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.DataProcessor.ExportDto
+    {
+    public class ExportSellersDTO
+        {
+        public string Name { get; set; }
+        public string Website { get; set; }
+
+        public AllBoardgame[] Boardgames { get; set; }
+    }
+
+    public class AllBoardgame
+        {
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public string Mechanics { get; set; }
+
+        [EnumDataType(typeof(CategoryType))]
+        public CategoryType Category { get; set; }
+    }
+    }
--- examPrep/2/Boardgames/DataProcessor/Serializer.cs
+++ examPrep/2/Boardgames/DataProcessor/Serializer.cs
@@ -1,17 +1,45 @@
 ﻿namespace Boardgames.DataProcessor
-{
+    {
     using Boardgames.Data;
+    using Boardgames.DataProcessor.ExportDto;
+    using Invoices.Extentions;
 
     public class Serializer
-    {
-        public static string ExportCreatorsWithTheirBoardgames(BoardgamesContext context)
         {
-            throw new NotImplementedException();
-        }
+        public static string ExportCreatorsWithTheirBoardgames(BoardgamesContext context)
+            {
+            return "";
+            }
 
         public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
-        {
-            throw new NotImplementedException();
+            {
+            var result = context.Sellers
+                .Where(s => s.BoardgamesSellers
+                    .Any(bs => bs.Boardgame.YearPublished >= year
+                        && bs.Boardgame.Rating <= rating))
+                .Select(s => new ExportSellersDTO
+                    {
+                    Name = s.Name,
+                    Website = s.Website,
+                    Boardgames = s.BoardgamesSellers
+                    .Where((bs => bs.Boardgame.YearPublished >= year
+                        && bs.Boardgame.Rating <= rating)).Select(bs => new AllBoardgame
+                        {
+                        Name = bs.Boardgame.Name,
+                        Rating = bs.Boardgame.Rating,
+                        Mechanics = bs.Boardgame.Mechanics,
+                        Category = bs.Boardgame.CategoryType
+                        })
+                    .OrderByDescending(s => s.Rating)
+                    .ThenBy(s => s.Name)
+                    .ToArray()
+                    })
+                .OrderByDescending(s => s.Boardgames.Count())
+                .ThenBy(s => s.Name)
+                .Take(5)
+                .ToArray();
+
+            return result.SerializeToJson();
+            }
         }
     }
-}
