5970b0bdcec5ebb010b123680a75dd5c3e0cc46f
DanAleksiev <toksikar46@gmail.com>
2023-12-04 19:00:19 +0000

    ...

--- interviewPrep/interviewPrep/Program.cs
+++ interviewPrep/interviewPrep/Program.cs
@@ -7,47 +7,102 @@ namespace interviewPrep
         {
         static void Main(string[] args)
             {
-            //capitalise the first letter in every word
-            string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            Console.WriteLine($"Capitalise: {lorem}");
-            Console.WriteLine(Capitalise(lorem));
+            ////capitalise the first letter in every word
+            //string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            //Console.WriteLine($"Capitalise: {lorem}");
+            //Console.WriteLine(Capitalise(lorem));
 
-            //find the common numbers between the 2 arrays
-            int[] firstArrWithCommonInt = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, };
-            int[] secondArrWithCommonInt = { 1, 7, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, };
-            int[] commonElements = GetComonElements(firstArrWithCommonInt, secondArrWithCommonInt);
-            Console.WriteLine(string.Join(" ", commonElements));
+            ////find the common numbers between the 2 arrays
+            //int[] firstArrWithCommonInt = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, };
+            //int[] secondArrWithCommonInt = { 1, 7, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, };
+            //int[] commonElements = GetComonElements(firstArrWithCommonInt, secondArrWithCommonInt);
+            //Console.WriteLine(string.Join(" ", commonElements));
 
 
             //TODO: sorting alorytms !!!
             int[] sortTheArray = { 10, 4, 6, 1, 2, 5, 7, 8, 3, 9, };
-            int[] sortedArray = { 1, 2, 3,4,5,6,7,8,9,10};
+            int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            Console.WriteLine(QuickSortAlgorithm(sortedArray,sortTheArray));
+            Console.WriteLine(SortList(sortedArray, sortTheArray));
+            Console.WriteLine(SortedList(sortedArray, sortTheArray));
+            Console.WriteLine(QuickSortAlgorithm(sortedArray, sortTheArray));
 
-        }
+            }
+
+        private static bool SortedList(int[] sortedArray, int[] sortTheArray)
+            {
+            int[] unsortedArray = sortTheArray;
+            StringBuilder sb = new StringBuilder();
+
+            Stopwatch speed = new Stopwatch();
+            speed.Start();
+
+            List<int> sortedList = sortTheArray.ToList();
+            sortedList.Sort();
+
+            speed.Stop();
+
+            sb.AppendLine("The array is sorted with Sort List algorithm:");
+            sb.AppendLine($"    It was :{string.Join("", unsortedArray)}");
+            sb.AppendLine($"    Output :{string.Join("", sortTheArray)}");
+            sb.AppendLine($"    Expected :{string.Join("", sortedArray)}");
+            sb.AppendLine($"{speed.ElapsedTicks}");
+            sb.AppendLine($"....................................................");
+            return sb.ToString();
+            }
+
+        private static string SortList(int[] sortedArray, int[] sortTheArray)
+            {
+            int[] unsortedArray = sortTheArray;
+            StringBuilder sb = new StringBuilder();
+
+            Stopwatch speed = new Stopwatch();
+            speed.Start();
+            
+            List<int> sortedList = sortTheArray.ToList();
+            sortedList.Sort();
+
+            speed.Stop();
+
+            sb.AppendLine("The array is sorted with Sort List algorithm:");
+            sb.AppendLine($"    It was :{string.Join("", unsortedArray)}");
+            sb.AppendLine($"    Output :{string.Join("", sortTheArray)}");
+            sb.AppendLine($"    Expected :{string.Join("", sortedArray)}");
+            sb.AppendLine($"{speed.ElapsedTicks}");
+            sb.AppendLine($"....................................................");
+            return sb.ToString();
+            }
 
         private static string QuickSortAlgorithm(int[] sortedArray, int[] sortTheArray)
             {
+            StringBuilder sb = new StringBuilder();
+            int[] unsortedArray = sortTheArray;
+
             Stopwatch speed = new Stopwatch();
             speed.Start();
-            for (int i = 0; i < sortTheArray.Length; i++)
-            {
-                for (int j = sortTheArray.Length - 1; i > 0; i--)
+
+            for (int i = 0; i < unsortedArray.Length; i++)
+                {
+                for (int j = unsortedArray.Length-1; j > i; j--)
                     {
-                    if (sortTheArray[i] > sortedArray[j])
+                    if (unsortedArray[i] > unsortedArray[j])
                         {
-                        int temp = sortTheArray[i];
-                        sortTheArray[i] = sortTheArray[j];
-                        sortTheArray[j] = temp;
+                        int tempJ = unsortedArray[j];
+                        int tempI = unsortedArray[i];
+                        unsortedArray[i] = tempJ;
+                        unsortedArray[j] = tempI;
+                        
                         }
                     }
-            }
+                }
             speed.Stop();
-            Console.WriteLine("The array is sorted with Quicksort algorithm:");
-            Console.WriteLine($"    Output :{sortTheArray}");
-            Console.WriteLine($"    Expected :{sortedArray}");
-            return speed.ToString();
+            sb.AppendLine("The array is sorted with Quicksort algorithm:");
+            sb.AppendLine($"    It was :{string.Join("", unsortedArray)}");
+            sb.AppendLine($"    Output :{string.Join("", sortTheArray)}");
+            sb.AppendLine($"    Expected :{string.Join("", sortedArray)}");
+            sb.AppendLine($"{speed.ElapsedTicks}");
+            sb.AppendLine($"....................................................");
+            return sb.ToString();
             }
 
         public static string Capitalise(string input)
