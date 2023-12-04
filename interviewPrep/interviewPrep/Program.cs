9c38d7f93bb15274f859976f40661edd27cc3908
DanAleksiev <toksikar46@gmail.com>
2023-12-04 18:16:47 +0000

    ...

--- interviewPrep/interviewPrep/Program.cs
+++ interviewPrep/interviewPrep/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 
 namespace interviewPrep
     {
@@ -6,25 +7,47 @@ namespace interviewPrep
         {
         static void Main(string[] args)
             {
-            string input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            //capitalise the first letter in every word
+            string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            Console.WriteLine($"Capitalise: {lorem}");
+            Console.WriteLine(Capitalise(lorem));
 
-            //Console.WriteLine(Capitalise(input));
-
-            int[] firstArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, };
-
-            int[] secondArr = { 1, 7, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, };
-
-
-            int[] commonElements = GetComonElements(firstArr, secondArr);
+            //find the common numbers between the 2 arrays
+            int[] firstArrWithCommonInt = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, };
+            int[] secondArrWithCommonInt = { 1, 7, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, };
+            int[] commonElements = GetComonElements(firstArrWithCommonInt, secondArrWithCommonInt);
             Console.WriteLine(string.Join(" ", commonElements));
+
+
             //TODO: sorting alorytms !!!
+            int[] sortTheArray = { 10, 4, 6, 1, 2, 5, 7, 8, 3, 9, };
+            int[] sortedArray = { 1, 2, 3,4,5,6,7,8,9,10};
+
+            Console.WriteLine(QuickSortAlgorithm(sortedArray,sortTheArray));
 
         }
 
-        private static int[] GetComonElements(int[] firstArr, int[] secondArr)
+        private static string QuickSortAlgorithm(int[] sortedArray, int[] sortTheArray)
             {
-            int[] thirdArr = firstArr.Intersect(secondArr).ToArray();
-            return thirdArr;
+            Stopwatch speed = new Stopwatch();
+            speed.Start();
+            for (int i = 0; i < sortTheArray.Length; i++)
+            {
+                for (int j = sortTheArray.Length - 1; i > 0; i--)
+                    {
+                    if (sortTheArray[i] > sortedArray[j])
+                        {
+                        int temp = sortTheArray[i];
+                        sortTheArray[i] = sortTheArray[j];
+                        sortTheArray[j] = temp;
+                        }
+                    }
+            }
+            speed.Stop();
+            Console.WriteLine("The array is sorted with Quicksort algorithm:");
+            Console.WriteLine($"    Output :{sortTheArray}");
+            Console.WriteLine($"    Expected :{sortedArray}");
+            return speed.ToString();
             }
 
         public static string Capitalise(string input)
@@ -45,6 +68,10 @@ namespace interviewPrep
             return sb.ToString().Trim();
             }
 
-        
+        private static int[] GetComonElements(int[] firstArr, int[] secondArr)
+            {
+            int[] thirdArr = firstArr.Intersect(secondArr).ToArray();
+            return thirdArr;
+            }
         }
     }
